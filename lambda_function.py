8bd24dfb13f29478524d54a44038f37ae453eacd
nakatest1234 <ambient2step@gmail.com>
2020-02-28 08:27:41 +0900

    1ファイルにまとめ、アラーム別に処理を分離

--- lambda_function.py
+++ lambda_function.py
@@ -2,30 +2,34 @@ import os
 import sys
 import json
 import urllib.request, urllib.error
+import re
 
+# handler
 def lambda_handler(event, context):
+	# check keys
+	if (event.get('Records')):
+		if (len(event.get('Records'))):
+			Message = event.get('Records')[0].get('Sns', {}).get('Message')
+			if (Message is None):
+				return -1
+			else:
+				process(Message)
 
-	msg = json.loads(event['Records'][0]['Sns']['Message'])
+	return 0
 
-	attachments = [
-		{
-			'color': 'good' if (msg['NewStateValue']=='OK') else 'danger',
-			'title': msg['AlarmDescription'],
-			'text': msg['StateChangeTime']+'(UTC)',
-			'fields': [
-				{
-					'title': 'ステータス',
-					'value': '{old}->{new}'.format(old=msg['OldStateValue'],new=msg['NewStateValue']),
-					'short': False,
-				},
-				{
-					'title': '理由',
-					'value': '```'+msg['NewStateReason']+'```',
-					'short': False
-				},
-			],
-		}
-	];
+
+# main
+def process(Message):
+	msg = json.loads(Message)
+
+	if (msg.get('source')=='aws.batch'):
+		print('process: aws.batch')
+		attachments = get_attachments_batch(msg)
+	elif ('AlarmName' in msg):
+		print('process: alarm')
+		attachments = get_attachments_alarm(msg)
+	else:
+		return
 
 	SLACK_ENDPOINT = os.environ.get('SLACK_ENDPOINT', '')
 	SLACK_CHANNEL  = os.environ.get('SLACK_CHANNEL', '#general')
@@ -34,8 +38,83 @@ def lambda_handler(event, context):
 
 	slack(SLACK_ENDPOINT, SLACK_CHANNEL , {'attachments':attachments,'username':SLACK_USERNAME,'icon_emoji':SLACK_EMOJI});
 
-	return []
+	return
 
+
+# alarm
+def get_attachments_alarm(msg):
+	attachments = [
+		{
+			'color': 'good' if (msg['NewStateValue']=='OK') else 'danger',
+			'text': msg['StateChangeTime']+'(UTC)',
+			'fields': [
+				{
+					'title': msg['AlarmName'],
+					'value': msg['AlarmDescription'],
+					'short': True,
+				},
+				{
+					'title': 'ステータス',
+					'value': '{old}->{new}'.format(old=msg['OldStateValue'],new=msg['NewStateValue']),
+					'short': True,
+				},
+				{
+					'value': '```'+msg['NewStateReason']+'```',
+					'short': False
+				},
+			],
+		}
+	];
+
+	return attachments
+
+
+# batch
+def get_attachments_batch(msg):
+	detail = msg['detail']
+	re_colon = re.compile(r'(?:.*?):(?:.*?):(?:.*?):(?:.*?):(?:.*?):(.*)')
+
+	attachments = [
+		{
+			'color': 'danger',
+			'text': msg['time']+'(UTC)',
+			'fields': [
+				{
+					'title': 'jobName',
+					'value': detail['jobName'],
+					'short': True,
+				},
+				{
+					'title': 'statusReason',
+					'value': detail['statusReason'],
+					'short': True,
+				},
+				{
+					'title': 'jobQueue',
+					'value': re_colon.sub(r'\1', detail['jobQueue']),
+					'short': True,
+				},
+				{
+					'title': 'jobDef',
+					'value': re_colon.sub(r'\1', detail['jobDefinition']),
+					'short': True,
+				},
+				{
+					'value': '```'+str(detail['container']['command'])+'```',
+					'short': False,
+				},
+				{
+					'value': '```'+detail['container']['reason']+'```',
+					'short': False
+				},
+			],
+		}
+	];
+
+	return attachments
+
+
+# slack
 def slack(url, channel, opt=None):
 	payload = {
 		'channel': channel,
